b241eac2064373bba0bf541ecfacd0d7867ca530
ybhas <ybhas@Bhaskar>
2025-01-26 20:10:12 +0530

    First Rest Program

---
+++ Web_Platform_For_Advisor_Trading/src/test/java/RestAssured/FirstRestProgram.java
@@ -0,0 +1,12 @@
+package RestAssured;
+
+import io.restassured.RestAssured;
+
+public class FirstRestProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		RestAssured.baseURI="";
+	}
+
+}
